c95702bd99cfcb0331b0af632fb101aeb5167b0f
damolitionn <87623949+damolitionn@users.noreply.github.com>
2024-08-03 05:14:28 -0500

    added pre mouser one-two paw support

--- M1S Quadruple Crossing.cs
+++ M1S Quadruple Crossing.cs
@@ -20,17 +20,53 @@ public class M1_Quadruple_Crossing : SplatoonScript
 
     public override Metadata? Metadata => new(1, "damolitionn");
 
-    Element? PlayerBaits;
     private bool IsRegular = false;
-    private bool IsLeaping = false;
+    private bool IsLeapingCone = false;
+    private bool IsLeapingCleave = false;
+    private bool LeftFirst = false;
+    private bool RightFirst = false;
+    private bool PreMouser = false;
+    private bool PostMouser = false;
     private TickScheduler? sched = null;
+    private List<(string, string)> buffPairs = new List<(string, string)>();
 
     IBattleNpc? BlackCat => Svc.Objects.FirstOrDefault(x => x is IBattleNpc b && b.DataId == 17193 && b.IsTargetable) as IBattleNpc;
     IBattleNpc? JumpMarker => Svc.Objects.FirstOrDefault(x => x is IBattleNpc b && b.DataId == 17195 && !b.IsTargetable) as IBattleNpc;
+    IBattleNpc? Clone => Svc.Objects.FirstOrDefault(x => x is IBattleNpc b && b.DataId == 17196 && !b.IsTargetable && b.IsCharacterVisible()) as IBattleNpc;
 
     public override void OnSetup()
     {
-        //Snapshotted Cones
+        //Cleaves
+        if (!Controller.TryRegisterElement("LeftCleave", new(0)
+        {
+            Name = "LeftCleave",
+            Enabled = false,
+            type = 3,
+            refY = 30.0f,
+            radius = 30.0f,
+            refActorComparisonType = 3,
+            includeRotation = true,
+            AdditionalRotation = 4.712389f
+        }))
+        {
+            DuoLog.Error("Could not register layout");
+        }
+        if (!Controller.TryRegisterElement("RightCleave", new(0)
+        {
+            Name = "RightCleave",
+            Enabled = false,
+            type = 3,
+            refY = 30.0f,
+            radius = 30.0f,
+            refActorComparisonType = 3,
+            includeRotation = true,
+            AdditionalRotation = 1.5707964f
+        }))
+        {
+            DuoLog.Error("Could not register layout");
+        }
+
+        //Snapshot Clones
         Controller.RegisterElementFromCode("Snapshots", "{\"Name\":\"Snapshots\",\"type\":4,\"radius\":30.0,\"coneAngleMin\":-22,\"coneAngleMax\":22,\"color\":4278190335,\"fillIntensity\":0.3,\"originFillColor\":838861055,\"endFillColor\":838861055,\"thicc\":3.0,\"refActorNPCNameID\":12686,\"refActorRequireCast\":true,\"refActorCastId\":[37952, 37980],\"refActorComparisonType\":6,\"includeRotation\":true,\"DistanceMax\":13.2,\"FillStep\":4.0}");
 
         //Baited Cones
@@ -60,6 +96,21 @@ public class M1_Quadruple_Crossing : SplatoonScript
         }
     }
 
+    public override void OnVFXSpawn(uint target, string vfxPath) 
+    
+    { 
+        DuoLog.Debug($"VFX Spawned: Target:{target.GetObject().Name} VFX:{vfxPath}");
+    }
+
+
+    public override void OnTetherCreate(uint source, uint target, uint data2, uint data3, uint data5)
+    {
+        if (source.GetObject().DataId == 17196 && target.GetObject().DataId == 17193)
+        {
+            CheckBossBuffs();
+        }
+    }
+
     public override void OnMessage(string Message)
     {
         //Regular Quadruple Crossing
@@ -79,14 +130,210 @@ public class M1_Quadruple_Crossing : SplatoonScript
             sched?.Dispose();
             sched = new TickScheduler(() =>
             {
-                IsLeaping = true;
+                IsLeapingCone = true;
                 sched = new TickScheduler(() =>
                 {
-                    IsLeaping = false;
+                    IsLeapingCone = false;
                     HideCones();
                 }, 4500);
             }, 6000);
         }
+
+        //Left First Cleave
+        if (Message.Contains("(12686>37945)"))
+        {
+            IsLeapingCleave = false;
+            LeftFirst = true;
+            HandleCleaveSequence();
+        }
+
+        //Leaping Right First Cleave
+        /*if (Message.Contains("(12686>37965)"))
+        {
+            IsLeapingCleave = true;
+            RightFirst = true;
+            HandleCleaveSequence();
+        }*/
+
+
+        //Right First Cleave
+        if (Message.Contains("(12686>37942)"))
+        {
+            IsLeapingCleave = false;
+            RightFirst = true;
+            HandleCleaveSequence();
+        }
+    }
+
+    private void HandleCloneAttacks()
+    {
+        if (PreMouser == true)
+        {
+            if (LeftFirst)
+            {
+                LeftCleaveFirst(20000, 4000, 17196, 17196);
+            }
+            else
+            {
+                RightCleaveFirst(20000, 4000, 17196, 17196);
+            }
+        }
+    }
+
+    private void HandleCleaveSequence()
+    {
+        if (IsLeapingCleave == true)
+        {
+            if (LeftFirst)
+            {
+                LeftCleaveFirst(7000, 3000, 17195, 17193);
+            }
+            else
+            {
+                RightCleaveFirst(7000, 3000, 17195, 17193);
+            }
+
+        }
+        else
+        {
+            if (LeftFirst)
+            {
+                LeftCleaveFirst(6000, 3000, 17193, 17193);
+            }
+            else
+            {
+                RightCleaveFirst(6000, 3000, 17193, 17193);
+            }
+        }
+
+    }
+
+    private void LeftCleaveFirst(uint t1, uint t2, uint dataID1, uint dataID2)
+    {
+        if (Controller.TryGetElementByName("LeftCleave", out var leftCleave))
+        {
+            leftCleave.Enabled = true;
+            leftCleave.refActorDataID = dataID1;
+            leftCleave.onlyVisible = true;
+        }
+
+        sched?.Dispose();
+        sched = new TickScheduler(() =>
+        {
+            if (Controller.TryGetElementByName("LeftCleave", out var leftCleave))
+            {
+                leftCleave.Enabled = false;
+            }
+            if (Controller.TryGetElementByName("RightCleave", out var rightCleave))
+            {
+                rightCleave.Enabled = true;
+                rightCleave.refActorDataID = dataID2;
+                rightCleave.onlyVisible = true;
+            }
+
+            sched = new TickScheduler(() =>
+            {
+                if (Controller.TryGetElementByName("RightCleave", out var rightCleave))
+                {
+                    rightCleave.Enabled = false;
+                }
+            }, t2);
+        }, t1);
+    }
+
+    private void RightCleaveFirst(uint t1, uint t2, uint dataID1, uint dataID2)
+    {
+        if (Controller.TryGetElementByName("RightCleave", out var rightCleave))
+        {
+            rightCleave.Enabled = true;
+            rightCleave.refActorDataID = dataID1;
+            rightCleave.onlyVisible = true;
+        }
+
+        sched?.Dispose();
+        sched = new TickScheduler(() =>
+        {
+            if (Controller.TryGetElementByName("RightCleave", out var rightCleave))
+            {
+                rightCleave.Enabled = false;
+            }
+            if (Controller.TryGetElementByName("LeftCleave", out var leftCleave))
+            {
+                leftCleave.Enabled = true;
+                leftCleave.refActorDataID = dataID2;
+                leftCleave.onlyVisible = true;
+            }
+
+            sched = new TickScheduler(() =>
+            {
+                if (Controller.TryGetElementByName("LeftCleave", out var leftCleave))
+                {
+                    leftCleave.Enabled = false;
+                }
+            }, t2);
+        }, t1);
+    }
+
+    private void CheckBossBuffs()
+    {
+        var boss = BlackCat;
+        if (boss == null) { return; }
+        string movement = "";
+        string attack = "";
+
+        if (boss.StatusList.Any(status => status.StatusId == 4050))
+        {
+            movement = "Right";
+        }
+        else if (boss.StatusList.Any(status => status.StatusId == 4051))
+        {
+            movement = "Left";
+        }
+        if (boss.StatusList.Any(status => status.StatusId == 4048))
+        {
+            attack = "Cleave";
+        }
+        else if (boss.StatusList.Any(status => status.StatusId == 4049))
+        {
+            attack = "Claw";
+        }
+        if (!string.IsNullOrEmpty(movement) && !string.IsNullOrEmpty(attack))
+        {
+            PostMouser = true;
+            PreMouser = false;
+            buffPairs.Add((movement, attack));
+        }
+        if (string.IsNullOrEmpty(movement) && !string.IsNullOrEmpty(attack))
+        {
+            PreMouser = true;
+            PostMouser = false;
+            if (movement == "" && attack == "Cleave")
+            {
+                HandleCloneAttacks();
+            }
+        }
+    }
+
+    private void ShowCleaves()
+    {
+        if (LeftFirst)
+        {
+            if (Controller.TryGetElementByName("LeftCleave", out var e))
+            {
+                e.Enabled = true;
+                e.refActorDataID = 17193;
+            }
+
+        }
+        else
+        {
+            if (Controller.TryGetElementByName("RightCleave", out var e))
+            {
+                e.Enabled = true;
+                e.refActorDataID = 17193;
+            }
+        }
+
     }
 
     private void ShowCones()
@@ -96,7 +343,7 @@ public class M1_Quadruple_Crossing : SplatoonScript
         {
             boss = BlackCat;
         }
-        if (IsLeaping)
+        if (IsLeapingCone)
         {
             if (JumpMarker == null)
             {
@@ -142,7 +389,7 @@ public class M1_Quadruple_Crossing : SplatoonScript
 
     public override void OnUpdate()
     {
-        if (IsRegular || IsLeaping)
+        if (IsRegular || IsLeapingCone)
         {
             ShowCones();
         }
@@ -152,7 +399,7 @@ public class M1_Quadruple_Crossing : SplatoonScript
             if (boss == null || !Svc.Objects.Any(x => x.DataId == 17193 && x.IsTargetable))
             {
                 IsRegular = false;
-                IsLeaping = false;
+                IsLeapingCone = false;
                 HideCones();
             }
         }
@@ -166,9 +413,15 @@ public class M1_Quadruple_Crossing : SplatoonScript
     public override void OnCombatEnd()
     {
         IsRegular = false;
-        IsLeaping = false;
+        IsLeapingCone = false;
+        IsLeapingCleave = false;
+        LeftFirst = false;
+        RightFirst = false;
+        PreMouser = false;
+        PostMouser = false;
         HideCones();
         sched?.Dispose();
+        buffPairs.Clear();
     }
 
     public override void OnCombatStart()
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-M1S: Shows closest players for quadruple crossing (supports both leaping and non-leaping)
+M1S: 
+
+Draws cones on the 4 nearest players to the boss and on the snapshotted cones afterwards for regular and leaping quadruple crossing
+
+Draws rectangles for one-two paw and on its clones pre mouser.
+
 
 TODO:
-M1S: Add clone movements with nailchipper
+M1S: Add clone movements with nailchipper, need to differentiate between left and right first for Leaping one-two paw
