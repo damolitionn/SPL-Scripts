2700db606f8f08a0575d24aa0a416d979aba7afc
damolitionn <87623949+damolitionn@users.noreply.github.com>
2024-09-02 12:06:20 -0500

    update

--- R1S Multiscript.cs
+++ R1S Multiscript.cs
@@ -1,20 +1,13 @@
 ﻿using Dalamud.Game.ClientState.Objects.Types;
-using ECommons;
 using ECommons.DalamudServices;
-using ECommons.GameFunctions;
 using ECommons.Logging;
 using Splatoon.SplatoonScripting;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using Splatoon;
-using Dalamud.Game.ClientState.Objects.SubKinds;
-using FFXIVClientStructs.FFXIV.Client.Game.Object;
+
 using ECommons.Schedulers;
-using ECommons.Hooks.ActionEffectTypes;
-using Lumina.Data.Files;
-using Splatoon.Memory;
-using Lumina.Excel.GeneratedSheets2;
+
 
 namespace SplatoonScriptsOfficial.Duties.Dawntrail;
 public class R1S_Multiscript : SplatoonScript
@@ -23,24 +16,18 @@ public class R1S_Multiscript : SplatoonScript
 
     private List<Vector3> clonePositions = new List<Vector3>();
 
-    public override Metadata? Metadata => new(1, "damolitionn");
+    public override Metadata? Metadata => new(2, "damolitionn");
 
-    private bool IsRegular = false;
-    private bool IsChipper = false;
-    private bool IsLeapingCone = false;
     private bool IsLeapingCleave = false;
     private bool LeftFirst = false;
     private bool RightFirst = false;
     private string movement = "";
     private string attack = "";
-    private readonly string[] Leaping = { "(12686>37975)", "(12686>37976)", "(12686>38009)", "(12686>38010)", "(12686>38011)", "(12686>38012)", "(12686>38959)", "(12686>38995)" };
 
     private TickScheduler? sched = null;
     private Vector3 jumpTargetPosition;
 
     IBattleNpc? BlackCat => Svc.Objects.FirstOrDefault(x => x is IBattleNpc b && b.DataId == 17193 && b.IsTargetable) as IBattleNpc;
-    IBattleNpc? JumpMarker => Svc.Objects.FirstOrDefault(x => x is IBattleNpc b && b.DataId == 17195 && !b.IsTargetable) as IBattleNpc;
-    IBattleNpc? Clone => Svc.Objects.FirstOrDefault(x => x is IBattleNpc b && b.DataId == 17196) as IBattleNpc;
 
     public override void OnSetup()
     {
@@ -77,103 +64,6 @@ public class R1S_Multiscript : SplatoonScript
         {
             DuoLog.Error("Could not register layout");
         }
-        //Nailchipper AOEs
-        Controller.RegisterElementFromCode("NailchipperAOE", "{\"Name\":\"NailchipperAOE\",\"type\":1,\"radius\":5.0,\"color\":4278190335,\"fillIntensity\":0.3,\"originFillColor\":838861055,\"endFillColor\":838861055,\"refActorComparisonType\":7,\"includeRotation\":true,\"FaceMe\":true,\"refActorVFXPath\":\"vfx/lockon/eff/lockon8_t0w.avfx\",\"refActorVFXMax\":8000,\"refActorTetherTimeMin\":0.0,\"refActorTetherTimeMax\":0.0}");
-        //Snapshot Clones
-        Controller.RegisterElementFromCode("Snapshots", "{\"Name\":\"Snapshots\",\"enabled\": true, \"type\":4,\"radius\":30.0,\"coneAngleMin\":-22,\"coneAngleMax\":22,\"color\":4278190335,\"fillIntensity\":0.3,\"originFillColor\":838861055,\"endFillColor\":838861055,\"thicc\":3.0,\"refActorNPCNameID\":12686,\"refActorRequireCast\":true,\"refActorCastId\":[37952, 37980],\"refActorComparisonType\":6,\"includeRotation\":true,\"DistanceMax\":13.2,\"FillStep\":4.0}");
-
-        //Baited Cones
-        for (int i = 0; i < 4; i++)
-        {
-            if (!Controller.TryRegisterElement($"PlayerConeBait{i}", new(0)
-            {
-                Name = "",
-                Enabled = false,
-                type = 4,
-                radius = 30.0f,
-                coneAngleMin = -22,
-                coneAngleMax = 22,
-                color = 4278190335,
-                fillIntensity = 0.3f,
-                originFillColor = 1677721855,
-                endFillColor = 1677721855,
-                thicc = 3.0f,
-                refActorDataID = 17193,
-                refActorComparisonType = 3,
-                includeRotation = true,
-                FaceMe = true,
-            }))
-            {
-                DuoLog.Error("Could not register layout");
-            }
-        }
-
-        //Clone Cones
-        for (int i = 0; i < 4; i++)
-        {
-            if (!Controller.TryRegisterElement($"CloneCone{i}", new(0)
-            {
-                Name = "",
-                Enabled = false,
-                type = 5,
-                radius = 30.0f,
-                coneAngleMin = -22,
-                coneAngleMax = 22,
-                color = 4278190335,
-                fillIntensity = 0.3f,
-                originFillColor = 1677721855,
-                endFillColor = 1677721855,
-                thicc = 3.0f,
-                includeRotation = true,
-                FaceMe = true,
-            }))
-            {
-                DuoLog.Error("Could not register layout");
-            }
-        }
-    }
-
-
-    public override void OnMessage(string Message)
-    {
-        //NailChipper
-        if (Message.Contains("(12686>38021)"))
-        {
-            sched = new TickScheduler(() =>
-            {
-                IsChipper = true;
-                sched = new TickScheduler(() =>
-                {
-                    IsChipper = false;
-                    HideCones();
-                }, 7000);
-            }, 4000);
-        }
-
-        //Regular Quadruple Crossing
-        if (Message.Contains("(12686>37948)"))
-        {
-            IsRegular = true;
-            sched = new TickScheduler(() =>
-            {
-                IsRegular = false;
-                HideCones();
-            }, 10000);
-        }
-        //Leaping Quadruple Crossing
-        if (Leaping.Any(Leaping => Message.Contains(Leaping)))
-        {
-            
-            sched = new TickScheduler(() =>
-            {
-                IsLeapingCone = true;
-                sched = new TickScheduler(() =>
-                {
-                    IsLeapingCone = false;
-                    HideCones();
-                }, 4500);
-            }, 6000);
-        }
     }
 
     public override void OnVFXSpawn(uint target, string vfxPath)
@@ -204,10 +94,6 @@ public class R1S_Multiscript : SplatoonScript
 
         if (obj?.DataId == 17196 && clonePositions.Count == 9)
         {
-            if (vfxPath.Contains("vfx/common/eff/mon_eisyo03t.avfx"))
-            {
-                GetJumpPositions(8);
-            }
             if (vfxPath.Contains("vfx/common/eff/m0884_cast_dbl01p1.avfx"))
             {
                 GetJumpPositions(8);
@@ -217,39 +103,10 @@ public class R1S_Multiscript : SplatoonScript
 
         if (obj?.DataId == 17196 && clonePositions.Count == 10)
         {
-            if (vfxPath.Contains("vfx/common/eff/mon_eisyo03t.avfx"))
-            {
-                GetJumpPositions(9);
-            }
             if (vfxPath.Contains("vfx/common/eff/m0884_cast_dbl01p1.avfx"))
             {
                 GetJumpPositions(9);
                 HandleCloneCleaves();
-            }
-        }
-    }
-
-    private void HandleCloneCones()
-    {
-        var jumpPosition = jumpTargetPosition;
-        var partyList = FakeParty.Get().ToList();
-        var players = Svc.Objects
-            .OfType<IPlayerCharacter>()
-            .OrderBy(obj => Vector3.Distance(jumpPosition, obj.Position))
-            .Take(4)
-            .ToList();
-
-        for (int i = 0; i < players.Count; i++)
-        {
-            var player = players[i];
-            var partyIndex = partyList.FindIndex(p => p.Name.TextValue == player.Name.TextValue);
-            if (Controller.TryGetElementByName($"CloneCone{i}", out var e))
-            {
-                e.Enabled = true;
-                e.refX = jumpPosition.X;
-                e.refY = jumpPosition.Z;
-                e.refZ = jumpPosition.Y;
-                e.faceplayer = $"<{(partyIndex + 1)}>";
             }
         }
     }
@@ -335,80 +192,6 @@ public class R1S_Multiscript : SplatoonScript
                 RightCleaveFirst(6000, 3000, 17193, 17193);
             }
         }
-    }
-
-    private void RightCloneCleave()
-    {
-        if (Controller.TryGetElementByName("CloneCleave", out var rightCleave))
-        {
-            rightCleave.Enabled = true;
-            rightCleave.refX = jumpTargetPosition.X;
-            rightCleave.refY = jumpTargetPosition.Z;
-            rightCleave.offX = jumpTargetPosition.X + 30f;
-            rightCleave.offY = jumpTargetPosition.Z;
-        }
-
-        sched?.Dispose();
-        sched = new TickScheduler(() =>
-        {
-            if (Controller.TryGetElementByName("CloneCleave", out var rightCleave))
-            {
-                rightCleave.Enabled = false;
-            }
-            if (Controller.TryGetElementByName("CloneCleave", out var leftCleave))
-            {
-                leftCleave.Enabled = true;
-                leftCleave.refX = jumpTargetPosition.X;
-                leftCleave.refY = jumpTargetPosition.Z;
-                leftCleave.offX = jumpTargetPosition.X - 30f;
-                leftCleave.offY = jumpTargetPosition.Z;
-            }
-
-            sched = new TickScheduler(() =>
-            {
-                if (Controller.TryGetElementByName("CloneCleave", out var leftCleave))
-                {
-                    leftCleave.Enabled = false;
-                }
-            }, 2000);
-        }, 7000);
-    }
-
-    private void LeftCloneCleave()
-    {
-        if (Controller.TryGetElementByName("CloneCleave", out var leftCleave))
-        {
-            leftCleave.Enabled = true;
-            leftCleave.refX = jumpTargetPosition.X;
-            leftCleave.refY = jumpTargetPosition.Z;
-            leftCleave.offX = jumpTargetPosition.X - 30f;
-            leftCleave.offY = jumpTargetPosition.Z;
-        }
-
-        sched?.Dispose();
-        sched = new TickScheduler(() =>
-        {
-            if (Controller.TryGetElementByName("CloneCleave", out var leftCleave))
-            {
-                leftCleave.Enabled = false;
-            }
-            if (Controller.TryGetElementByName("CloneCleave", out var rightCleave))
-            {
-                rightCleave.Enabled = true;
-                rightCleave.refX = jumpTargetPosition.X;
-                rightCleave.refY = jumpTargetPosition.Z;
-                rightCleave.offX = jumpTargetPosition.X + 30f;
-                rightCleave.offY = jumpTargetPosition.Z;
-            }
-
-            sched = new TickScheduler(() =>
-            {
-                if (Controller.TryGetElementByName("CloneCleave", out var rightCleave))
-                {
-                    rightCleave.Enabled = false;
-                }
-            }, 2000);
-        }, 7000);
     }
 
     private void HandleCloneCleaves()
@@ -505,6 +288,80 @@ public class R1S_Multiscript : SplatoonScript
                 }
             }, t2);
         }, t1);
+    }
+
+    private void LeftCloneCleave()
+    {
+        if (Controller.TryGetElementByName("CloneCleave", out var leftCleave))
+        {
+            leftCleave.Enabled = true;
+            leftCleave.refX = jumpTargetPosition.X;
+            leftCleave.refY = jumpTargetPosition.Z;
+            leftCleave.offX = jumpTargetPosition.X - 30f;
+            leftCleave.offY = jumpTargetPosition.Z;
+        }
+
+        sched?.Dispose();
+        sched = new TickScheduler(() =>
+        {
+            if (Controller.TryGetElementByName("CloneCleave", out var leftCleave))
+            {
+                leftCleave.Enabled = false;
+            }
+            if (Controller.TryGetElementByName("CloneCleave", out var rightCleave))
+            {
+                rightCleave.Enabled = true;
+                rightCleave.refX = jumpTargetPosition.X;
+                rightCleave.refY = jumpTargetPosition.Z;
+                rightCleave.offX = jumpTargetPosition.X + 30f;
+                rightCleave.offY = jumpTargetPosition.Z;
+            }
+
+            sched = new TickScheduler(() =>
+            {
+                if (Controller.TryGetElementByName("CloneCleave", out var rightCleave))
+                {
+                    rightCleave.Enabled = false;
+                }
+            }, 2000);
+        }, 7000);
+    }
+
+    private void RightCloneCleave()
+    {
+        if (Controller.TryGetElementByName("CloneCleave", out var rightCleave))
+        {
+            rightCleave.Enabled = true;
+            rightCleave.refX = jumpTargetPosition.X;
+            rightCleave.refY = jumpTargetPosition.Z;
+            rightCleave.offX = jumpTargetPosition.X + 30f;
+            rightCleave.offY = jumpTargetPosition.Z;
+        }
+
+        sched?.Dispose();
+        sched = new TickScheduler(() =>
+        {
+            if (Controller.TryGetElementByName("CloneCleave", out var rightCleave))
+            {
+                rightCleave.Enabled = false;
+            }
+            if (Controller.TryGetElementByName("CloneCleave", out var leftCleave))
+            {
+                leftCleave.Enabled = true;
+                leftCleave.refX = jumpTargetPosition.X;
+                leftCleave.refY = jumpTargetPosition.Z;
+                leftCleave.offX = jumpTargetPosition.X - 30f;
+                leftCleave.offY = jumpTargetPosition.Z;
+            }
+
+            sched = new TickScheduler(() =>
+            {
+                if (Controller.TryGetElementByName("CloneCleave", out var leftCleave))
+                {
+                    leftCleave.Enabled = false;
+                }
+            }, 2000);
+        }, 7000);
     }
 
     private void CheckBossBuffs()
@@ -634,102 +491,9 @@ public class R1S_Multiscript : SplatoonScript
         }
 
     }
-    private void ShowCones()
-    {
-        var boss = BlackCat;
-        if (IsRegular)
-        {
-            boss = BlackCat;
-        }
-        if (IsLeapingCone)
-        {
-            if (JumpMarker == null)
-            {
-                boss = BlackCat;
-            }
-            else
-            {
-                boss = JumpMarker;
-            }
-        }
-
-        if (boss == null) { return; }
-
-        var partyList = FakeParty.Get().ToList();
-        var players = Svc.Objects
-            .OfType<IPlayerCharacter>()
-            .OrderBy(obj => Vector3.Distance(boss.Position, obj.Position))
-            .Take(4)
-            .ToList();
-
-        for (int i = 0; i < players.Count; i++)
-        {
-            var player = players[i];
-            var partyIndex = partyList.FindIndex(p => p.Name.TextValue == player.Name.TextValue);
-            if (Controller.TryGetElementByName($"PlayerConeBait{i}", out var e))
-            {
-                e.Enabled = true;
-                e.faceplayer = $"<{(partyIndex + 1)}>";
-            }
-        }
-    }
-
-    private void HideCones()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            if (Controller.TryGetElementByName($"PlayerConeBait{i}", out var e1))
-            {
-                e1.Enabled = false;
-            }
-
-            if (Controller.TryGetElementByName($"CloneCone{i}", out var e2))
-            {
-                e2.Enabled = false;
-            }
-        }
-    }
 
     public override void OnUpdate()
     {
-        if (clonePositions.Count <= 8 && (IsRegular || IsLeapingCone))
-        {
-            ShowCones();
-        }
-        else
-        {
-            var boss = BlackCat;
-            if (boss == null || !Svc.Objects.Any(x => x.DataId == 17193 && x.IsTargetable))
-            {
-                IsRegular = false;
-                IsLeapingCone = false;
-                HideCones();
-            }
-        }
-
-        if (IsChipper)
-        {
-            HandleCloneCones();
-        }
-        else
-        {
-            var boss = BlackCat;
-            if (boss == null || !Svc.Objects.Any(x => x.DataId == 17193 && x.IsTargetable))
-            {
-                IsChipper = false;
-                HideCones();
-            }
-        }
-
-        if (Controller.TryGetElementByName($"Snapshots", out var s))
-        {
-            s.Enabled = true;
-        }
-
-        if (Controller.TryGetElementByName($"NailchipperAOE", out var e))
-        {
-            e.Enabled = true;
-        }
         if (clonePositions.Count == 9)
         {
             sched = new TickScheduler(() =>
@@ -753,15 +517,11 @@ public class R1S_Multiscript : SplatoonScript
         }
     }
 
-    private void Reset()
+    public override void OnReset()
     {
-        IsRegular = false;
-        IsChipper = false;
-        IsLeapingCone = false;
         IsLeapingCleave = false;
         LeftFirst = false;
         RightFirst = false;
-        HideCones();
         sched?.Dispose();
         clonePositions.Clear();
         jumpTargetPosition = new Vector3(0, 0, 0);
@@ -775,16 +535,5 @@ public class R1S_Multiscript : SplatoonScript
         {
             e2.Enabled = false;
         }
-
-    }
-
-    public override void OnCombatEnd()
-    {
-        Reset();
-    }
-
-    public override void OnCombatStart()
-    {
-        Reset();
     }
 }
